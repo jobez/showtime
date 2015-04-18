3be2b958560aeee425483141db920802b7078d11
Johann Bestowrous <johann.bestowrous@gmail.com>
2015-04-18 16:53:57 -0400

    pass in container to performer scope

--- src/showtime/bad_jokes.cljs
+++ src/showtime/bad_jokes.cljs
@@ -2,29 +2,29 @@
   (:require-macros [cljs.core.async.macros :refer [go go-loop]])
   (:require [goog.dom.classes :as class]
             [showtime.main :as show :refer (IPerform IPrep IAsyncPrep IStageTime
-                                                      showtime
-                                                      end-performance
-                                                      start-performance
-                                                      awill-enter-stage
-                                                      awill-leave-stage
-                                                      will-enter-stage
-                                                      will-leave-stage
-                                                      perf-time
-                                                      entrance-slack)]
+                                                     showtime
+                                                     end-performance
+                                                     start-performance
+                                                     awill-enter-stage
+                                                     awill-leave-stage
+                                                     will-enter-stage
+                                                     will-leave-stage
+                                                     perf-time
+                                                     entrance-slack)]
             [cljs-http.client :as http]
-             [cljs.core.async :as async :refer [<! alts! put!]]))
+            [cljs.core.async :as async :refer [<! alts! put!]]))
 
 (enable-console-print!)
 
-(defrecord CNJoke [text c]
+(defrecord CNJoke [text node container]
   IPerform
   (end-performance [this]
-    (aset c "innerHTML" (str "<i>" (:joke text) "</i>"))
-    (.. js/document (getElementById "joke-container") (appendChild c)))
+    (aset node "innerHTML" (str "<i>" (:joke text) "</i>"))
+    (.. container (appendChild node)))
   (start-performance [this]
-    (aset c "innerHTML" (str "<div id='joke'>" (:joke text) "</div>"))
-    (aset c "style" "fontSize" (str (+ 15 (rand-int 10))  "px"))
-    (.. js/document (getElementById "joke-container") (appendChild c)))
+    (aset node "innerHTML" (str "<div id='joke'>" (:joke text) "</div>"))
+    (aset node "style" "fontSize" (str (+ 15 (rand-int 10))  "px"))
+    (.. container (appendChild node)))
   IPrep
   (will-enter-stage [this])
   (will-leave-stage [this])
@@ -32,7 +32,7 @@
   (perf-time [this]
     (rand-int 5000)))
 
-(defn get-jokes [c b a]
+(defn get-jokes [node b a]
   (go-loop [jokes []]
     (<! (async/timeout 1000))
     (let [container (.. js/document (getElementById "joke-container"))]
@@ -43,7 +43,7 @@
             (aset container "innerHTML" (str "loading " (inc (count jokes)) " out of " 5 " jokes"))
             (recur (conj jokes joke)))
           (let [jokes (map (fn [joke]
-                             (->CNJoke joke c)) jokes)
+                             (->CNJoke joke node container)) jokes)
                 [tick close] (showtime jokes)]
             (aset container "innerHTML" (str "loaded " (count jokes) " out of " 5 " jokes"))
             (aset b "innerHTML" (str "<b>" "Stop the Bad Jokes" "</b>"))
